c97f619b17c8a6459e78a51abfba546c599ffdee
bakernpp <76157472+bakernpp@users.noreply.github.com>
2020-12-20 11:46:14 +0600

    Add files via upload

---
+++ 1124.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#define pi 2*acos(0.0)
+#define ll long long
+#define maxsz 2e5+10
+
+#define MAX(a,b)  ( ( a > b ) ? a : b )
+#define MIN(a,b)  ( ( a < b ) ? a : b )
+#define ABS(a)    ( ( a > 0 ) ? a : (-(a)))
+#define SQ(a) ( a * a )
+
+
+using namespace std;
+int main(){
+
+    //freopen("in.txt","r",stdin);
+    char str[1003];
+    while( gets(str)){
+
+        printf("%s\n", str);
+        //cout << "\n";
+
+    }
+    return 0;
+}
+
+
+
