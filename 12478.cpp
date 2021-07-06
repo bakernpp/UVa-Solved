d5968ea94e48d63790fd24778c29573dddea8d06
Md. Baker Hossen <76157472+bakernpp@users.noreply.github.com>
2021-07-06 13:06:31 +0600

    Add files via upload

---
+++ 12478.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<cstring>
+
+#define pi 2*cos(0.0)
+#define ll long long
+#define mxsz 2e5+10
+
+#define ABS(a) (a>0)? a : -(a)
+#define MAX(a,b) (a>b)? a : b
+#define MIN(a,b) (a<b)? a : b
+#define SQ(a) a*a
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //cout.tie(NULL);
+
+    cout << "RAKIBUL\n";
+
+    return 0;
+}
+
+
+
+
