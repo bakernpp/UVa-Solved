86f7c6478bde56548ed3de0d814f1a324b02f6de
Md. Baker Hossen <76157472+bakernpp@users.noreply.github.com>
2021-07-06 11:20:27 +0600

    Add files via upload

---
+++ 13025.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<cstring>
+
+#define pi 2*cos(0.0)
+#define ll long long
+#define mxsz 2e5+10
+
+#define ABS(a) (a>0)? a : -(a)
+#define MAX(a,b) (a>b)? a : b
+#define MIN(a,b) (a<b)? a : b
+#define SQ(a) a*a
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //cout.tie(NULL);
+
+    ll t, n;
+    cout << "May 29, 2013 Wednesday\n";
+    return 0;
+}
+
+
+
+
+
+
+
+
